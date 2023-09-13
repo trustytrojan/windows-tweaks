4e1cf2eaef4a6a341acc0a148fdde968ed1abf19
trustytrojan <87675609+trustytrojan@users.noreply.github.com>
2023-09-13 01:22:10 -0400

    Update remove-apps.ps1

--- dism/remove-apps.ps1
+++ dism/remove-apps.ps1
@@ -1,10 +1,10 @@
 param(
 	[Parameter(Mandatory)]
-	[string]$imagePath
+	[string]$MountedImagePath
 )
 
 $keepRegex = "Microsoft\.(DesktopAppInstaller|ScreenSketch|StorePurchaseApp|WindowsStore).+"
-$toRemove = Get-ProvisionedAppxPackage -Path $imagePath | ?{ $_.PackageName -notmatch $keepRegex } | %{ $_.PackageName }
+$toRemove = Get-ProvisionedAppxPackage -Path $MountedImagePath | ?{ $_.PackageName -notmatch $keepRegex } | %{ $_.PackageName }
 
 Write-Host "Removing the below apps:" -Background Blue
 $toRemove
@@ -17,7 +17,7 @@ if ((Read-Host) -notin "", "Y", "y") {
 
 foreach ($packageName in $toRemove) {
 	Write-Host "`nRemoving $packageName" -Foreground Blue
-	Remove-ProvisionedAppPackage -Path $imagePath -PackageName $packageName
+	Remove-ProvisionedAppxPackage -Path $MountedImagePath -PackageName $packageName
 }
 
 Write-Host "Finished removing apps!" -Foreground Green
