d18a73814f3ee88b28303a74ba510821d6838446
Tarun Rajan <87675609+trustytrojan@users.noreply.github.com>
2023-10-08 08:58:00 -0400

    Create advertising-id.ps1

---
+++ disable/advertising-id.ps1
@@ -0,0 +1,8 @@
+Write-Output "Disabling Advertising ID..."
+
+If (!(Test-Path "HKLM:\SOFTWARE\Policies\Microsoft\Windows\AdvertisingInfo")) {
+	New-Item -Path "HKLM:\SOFTWARE\Policies\Microsoft\Windows\AdvertisingInfo" | Out-Null
+}
+Set-ItemProperty -Path "HKLM:\SOFTWARE\Policies\Microsoft\Windows\AdvertisingInfo" -Name "DisabledByGroupPolicy" -Type DWord -Value 1 | Out-Null
+
+Write-Output "Done"
