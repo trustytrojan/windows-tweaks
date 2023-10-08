eb5ebf3ae429f13a9d8f2ebe7d6290ed0b8ed8c6
Tarun Rajan <87675609+trustytrojan@users.noreply.github.com>
2023-10-08 09:16:50 -0400

    Create new-context-menu.ps1

---
+++ disable/new-context-menu.ps1
@@ -0,0 +1,3 @@
+Write-Output "Disabling Windows 11 Right Click Menu..."
+New-Item -Path "HKCU:\Software\Classes\CLSID\{86ca1aa0-34aa-4e8b-a509-50c905bae2a2}" -Name "InprocServer32" -force -value ""
+Write-Output "Done"
