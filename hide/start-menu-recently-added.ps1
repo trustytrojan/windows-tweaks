e2407e73f6b84603fa70ca4ff3582cbd38f51284
Tarun Rajan <87675609+trustytrojan@users.noreply.github.com>
2023-10-08 09:03:08 -0400

    Create start-menu-recently-added.ps1

---
+++ hide/start-menu-recently-added.ps1
@@ -0,0 +1,8 @@
+Write-Output "Hiding 'Recently added' list from Start Menu..."
+
+If (!(Test-Path "HKLM:\SOFTWARE\Policies\Microsoft\Windows\Explorer")) {
+	New-Item -Path "HKLM:\SOFTWARE\Policies\Microsoft\Windows\Explorer" | Out-Null
+}
+Set-ItemProperty -Path "HKLM:\SOFTWARE\Policies\Microsoft\Windows\Explorer" -Name "HideRecentlyAddedApps" -Type DWord -Value 1 | Out-Null
+
+Write-Output "Done"
