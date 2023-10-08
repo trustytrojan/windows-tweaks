391ffa67b34731852abf691e8c7fe9efa4cc6cb5
Tarun Rajan <87675609+trustytrojan@users.noreply.github.com>
2023-10-08 08:49:01 -0400

    Update disable-features.ps1

--- dism/disable-features.ps1
+++ dism/disable-features.ps1
@@ -1,10 +1,10 @@
 param(
 	[Parameter(Mandatory)]
-	[string]$MountedImagePath
+	[string]$Path
 )
 
 $keepRegex = ".*RDC.*"
-$toDisable = Get-WindowsOptionalFeature -Path $MountedImagePath | ?{ ($_.State -eq "Enabled") -and ($_.FeatureName -notmatch $keepRegex) } | %{ $_.FeatureName }
+$toDisable = Get-WindowsOptionalFeature -Path $Path | ?{ ($_.State -eq "Enabled") -and ($_.FeatureName -notmatch $keepRegex) } | %{ $_.FeatureName }
 
 Write-Host "Disabling the below features:" -Background Blue
 $toDisable
@@ -17,7 +17,7 @@ if ((Read-Host) -notin "", "Y", "y") {
 
 foreach ($featureName in $toDisable) {
 	Write-Host "Disabling: $featureName" -Foreground Blue
-	Disable-WindowsOptionalFeature -Path $MountedImagePath -FeatureName $featureName
+	Disable-WindowsOptionalFeature -Path $Path -FeatureName $featureName
 }
 
 Write-Host "Finished disabling features!" -Foreground Green
