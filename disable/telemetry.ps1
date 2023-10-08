5cd6c6a6fae251dea40ee05197b93344fbc9f3e3
Tarun Rajan <87675609+trustytrojan@users.noreply.github.com>
2023-10-08 14:34:26 -0400

    Update telemetry.ps1

--- disable/telemetry.ps1
+++ disable/telemetry.ps1
@@ -1,4 +1,5 @@
 Write-Output "Disabling Telemetry..."
+
 Set-ItemProperty -Path "HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\DataCollection" -Name "AllowTelemetry" -Type DWord -Value 0 | Out-Null
 Set-ItemProperty -Path "HKLM:\SOFTWARE\Wow6432Node\Microsoft\Windows\CurrentVersion\Policies\DataCollection" -Name "AllowTelemetry" -Type DWord -Value 0 | Out-Null
 Set-ItemProperty -Path "HKLM:\SOFTWARE\Policies\Microsoft\Windows\DataCollection" -Name "AllowTelemetry" -Type DWord -Value 0 | Out-Null
