927eb680c83baef268b2b1c3487b24e87a95984d
Tarun Rajan <87675609+trustytrojan@users.noreply.github.com>
2023-10-08 17:14:35 -0400

    Update mount-registry.bat

--- dism/mount-registry.bat
+++ dism/mount-registry.bat
@@ -2,13 +2,13 @@
 
 if "%1"=="" (
 	echo You must provide a path to a Windows installation.
-	echo "Usage: %0 <image path>"
+	echo "Usage: %0 <path>"
 	exit
 )
 
 echo Mounting Image Registry for Offline Editing
-reg load HKLM\TK_COMPONENTS "%1\Windows\System32\config\COMPONENTS" >nul
-reg load HKLM\TK_DEFAULT "%1\Windows\System32\config\default" >nul
-reg load HKLM\TK_NTUSER "%1\Users\Default\ntuser.dat" >nul
-reg load HKLM\TK_SOFTWARE "%1\Windows\System32\config\SOFTWARE" >nul
-reg load HKLM\TK_SYSTEM "%1\Windows\System32\config\SYSTEM" >nul
+reg load HKLM\mount_COMPONENTS "%1\Windows\System32\config\COMPONENTS" >nul
+reg load HKLM\mount_DEFAULT "%1\Windows\System32\config\default" >nul
+reg load HKLM\mount_NTUSER "%1\Users\Default\ntuser.dat" >nul
+reg load HKLM\mount_SOFTWARE "%1\Windows\System32\config\SOFTWARE" >nul
+reg load HKLM\mount_SYSTEM "%1\Windows\System32\config\SYSTEM" >nul
