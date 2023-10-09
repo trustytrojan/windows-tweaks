7c58836c3c62f48b54781a8df96aa9286d5e2211
Tarun Rajan <87675609+trustytrojan@users.noreply.github.com>
2023-10-08 20:12:14 -0400

    Update and rename remove-apps.ps1 to Remove-AllApps.psm1

---
+++ Remove-AllApps.psm1
@@ -0,0 +1,24 @@
+function Remove-AllApps {
+	param(
+ 		[Parameter()]
+  		[string]$ExceptRegex = ""
+	)
+
+	$toRemove = Get-AppxPackage | ?{ $_.Name -notmatch $ExceptRegex }
+
+	Write-Host "Removing the below apps:" -Background Blue
+	$toRemove | %{ $_.Name }
+	Write-Host "Do you want to proceed? [Y/n]" -NoNewLine -Background Blue
+	Write-Host " " -NoNewLine
+
+	if ((Read-Host) -notin "", "Y", "y") {
+		exit
+	}
+
+	foreach ($package in $toRemove) {
+		Write-Host "Removing app: $($package.Name)" -Foreground Blue
+		Remove-AppxPackage $package
+	}
+
+	Write-Host "Finished removing apps!" -Foreground Green
+}
